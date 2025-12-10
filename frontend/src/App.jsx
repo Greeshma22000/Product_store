9a7248035186d855fa8599cafff7b819c57fdd83
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-10 15:48:02 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import {Button} from "@chakra-ui/react"
+import {Box, Button} from "@chakra-ui/react"
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
 
 const App = () => {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+      </Routes>
+    </Box>
   )
 }
 
